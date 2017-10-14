44432c446cb26785389bb44bcde6527f523cbe97
Robbie Gifford <robbie.gifford@gmail.com>
2017-10-13 19:52:36 -0700

    Use proper curl command for installing vim plugged

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Install junegunn/vim-plug pluggin manager
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugInstall +qall
